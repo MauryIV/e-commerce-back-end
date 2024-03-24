3db5f7902aa3e82ea1dd0b8fc3d7a140b8c118f7
MauryIV <mauryhughesiv@gmail.com>
2024-03-24 15:31:04 -0600

    updated all models. Included relationships. This has foreign keys, and created proper id set up with product name in accorance with the seeds provided.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ AS A manager at an internet retail company
 I WANT a back end for my e-commerce website that uses the latest technologies
 SO THAT my company can compete with other e-commerce companies
 
-
 Acceptance Criteria
 
 GIVEN a functional Express.js API
--- models/index.js
+++ models/index.js
@@ -1,20 +1,29 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
